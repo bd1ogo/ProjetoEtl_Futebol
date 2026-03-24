255f489c50d2f766e1eb5c42b5f7529e8472cf81
bd1ogo <barrosof.diogo@gmail.com>
2026-03-24 19:20:52 -0300

    feat(load): add JSON to MySQL data loading script

--- extract.py
+++ extract.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import datetime
 
 url = "https://www.thesportsdb.com/api/v1/json/3/eventspastleague.php?id=4328"
 
@@ -7,28 +8,28 @@ response = requests.get(url)
 response.status_code
 
 data = response.json()
-print(data)
 
 jogos = []
 
 for event in data["events"]:
+    league = event["strLeague"]
     home = event["strHomeTeam"]
     away = event["strAwayTeam"]
     home_score = event["intHomeScore"] or 0
     away_score = event["intAwayScore"] or 0
-    date = event["dateEvent"]
-    league = event["strLeague"]
+    date = datetime.strptime(event["dateEvent"], "%Y-%m-%d")
 
     jogo = {
         "liga": league,
-        "data": date,
+        "data": date.strftime("%Y-%m-%d"),
         "mandante": home,
         "visitante": away,
-        "placar": f"{home_score} x {away_score}"
+        "gols_mandante": home_score,
+        "gols_visitante": away_score
     }
 
     jogos.append(jogo)
-    print(jogos)
+    print(jogo)
 
-with open("jogos.json", "w") as f:
+with open("data/jogos.json", "w") as f:
     json.dump(jogos, f, indent=4)
---
+++ load.py
@@ -0,0 +1,50 @@
+import json
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host = "localhost",
+    port = 3307,
+    user = "user",
+    password = "password"
+)
+
+cursor = conexao.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS futebol")
+conexao.database = "futebol"
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS jogos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    liga VARCHAR(200),
+    mandante VARCHAR(100),
+    visitante VARCHAR(100),
+    gols_mandante INT,
+    gols_visitante INT,
+    data DATETIME
+)
+""")
+
+with open("data/jogos.json", "r", encoding="utf-8") as f:
+    jogos = json.load(f)
+
+for jogo in jogos:
+    sql = """
+    INSERT INTO jogos(liga, mandante, visitante, gols_mandante, gols_visitante, data)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+    valores = (
+        jogo["liga"],
+        jogo["mandante"],
+        jogo["visitante"],
+        jogo["gols_mandante"],
+        jogo["gols_visitante"],
+        jogo["data"]
+    )
+    cursor.execute(sql, valores)
+
+conexao.commit()
+print("Dados inseridos com sucesso!")
+
+cursor.close()
+conexao.close()
