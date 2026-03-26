73ac4db1c69db1b50d98620618ace023bef47778
bd1ogo <barrosof.diogo@gmail.com>
2026-03-26 18:00:17 -0300

    feat: complete dashboard and update main pipeline

--- analysis.py
+++ analysis.py
@@ -23,10 +23,6 @@ cursor.execute("""
 """)
 resultado_feitos = cursor.fetchall()
 
-print("\nTimes com mais gols feitos")
-for linha in resultado_feitos:
-    print(linha)
-
 cursor.execute("""
     SELECT time, SUM(gols) AS total_gols
     FROM (
@@ -39,48 +35,37 @@ cursor.execute("""
 """)
 resultado_sofridos = cursor.fetchall()
 
-print("\nTimes com mais gols sofridos")
-for linha in resultado_sofridos:
-    print(linha)
-
 cursor.execute("""
-    SELECT
-        feitos.time,
-        feitos.gols_feitos,
-        sofridos.gols_sofridos
-    FROM(
-        SELECT time, SUM(gols) AS gols_feitos
-        FROM (
-            SELECT mandante AS time, gols_mandante AS gols FROM jogos
-            UNION ALL
-            SELECT visitante AS time, gols_visitante AS gols FROM jogos
-            ) AS tabela
-            GROUP BY time
-        ) AS feitos
-        JOIN (
-            SELECT time, SUM(gols) AS gols_sofridos
-            FROM (
-                SELECT mandante AS time, gols_visitante AS gols FROM jogos
-                UNION ALL
-                SELECT visitante AS time, gols_mandante AS gols FROM jogos
-            ) AS tabela
-            GROUP BY time
-        ) AS sofridos
-        ON feitos.time = sofridos.time
-        ORDER BY feitos.gols_feitos DESC, sofridos.gols_sofridos ASC
+    SELECT 
+        time,
+        SUM(gols_feitos) AS gols_feitos,
+        SUM(gols_sofridos) AS gols_sofridos,
+        SUM(gols_feitos) - SUM(gols_sofridos) AS saldo_gols
+    FROM (
+        SELECT 
+            mandante AS time,
+            gols_mandante AS gols_feitos,
+            gols_visitante AS gols_sofridos
+    FROM jogos
+    UNION ALL
+    SELECT 
+        visitante AS time,
+        gols_visitante AS gols_feitos,
+        gols_mandante AS gols_sofridos
+    FROM jogos
+    ) AS tabela
+    GROUP BY time
+    ORDER BY saldo_gols DESC, gols_feitos DESC
 """)
 resultado = cursor.fetchall()
-
-print("\nRanking de Times")
-for linha in resultado:
-    print(linha)
 
 ranking = []
 for linha in resultado:
     ranking.append({
         "time": linha[0],
-        "gols_feitos": linha[1],
-        "gols_sofridos": linha[2]
+        "saldo_gols": linha[1],
+        "gols_feitos": linha[2],
+        "gols_sofridos": linha[3]
     })
 
 with open("data/ranking.json", "w", encoding="utf-8") as f:
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import os
 
-print("Iniciando pipeline")
+print("Iniciando Pipeline")
 os.system("python extract.py")
 
-print("Transformando dados")
+print("Transformando Dados")
 os.system("python transform.py")
 
-print("Carregando dados")
+print("Carregando Dados")
 os.system("python load.py")
+
+print("Analisando Dados")
+os.system("python analysis.py")
+
+print("Carregando Dashboard")
+os.system("streamlit run dashboard.py")
+
+print("\n Pipeline Finalizado")
