627405da1e7a1f11c87bcc80281c14176e6b047f
bd1ogo <barrosof.diogo@gmail.com>
2026-03-26 20:25:02 -0300

    refactor: restructure project into organized folders

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import os
 
 print("Iniciando Pipeline")
-os.system("python extract.py")
+os.system("python etl/extract.py")
 
 print("Transformando Dados")
-os.system("python transform.py")
+os.system("python etl/transform.py")
 
 print("Carregando Dados")
-os.system("python load.py")
+os.system("python etl/load.py")
 
 print("Analisando Dados")
-os.system("python analysis.py")
+os.system("python analysis/analysis.py")
 
 print("Carregando Dashboard")
-os.system("streamlit run dashboard.py")
+os.system("streamlit run dashboard/dashboard.py")
 
 print("\n Pipeline Finalizado")
